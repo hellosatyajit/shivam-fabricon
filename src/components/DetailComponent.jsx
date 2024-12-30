e7f8ae94f8438164f4ea4bf0beba053ff7c1b700
Mann <Mann@88.com>
2024-12-30 17:57:10 +0530

    svg arrow showed

--- src/components/DetailComponent.jsx
+++ src/components/DetailComponent.jsx
@@ -90,7 +90,10 @@ const DetailComponent = ({ data }) => {
               className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white text-4xl hover:text-gray-300"
               onClick={prevImage}
             >
-              <img src="/leftarrowgallery.svg" alt="Previous" />
+             <svg fill="#072F6A" height="30px" width="30px" viewBox="0 0 330 330" xmlns="http://www.w3.org/2000/svg">
+  <path d="M111.213,165.004L250.607,25.607c5.858-5.858,5.858-15.355,0-21.213c-5.858-5.858-15.355-5.858-21.213,0.001 l-150,150.004C76.58,157.211,75,161.026,75,165.004c0,3.979,1.581,7.794,4.394,10.607l150,149.996 C232.322,328.536,236.161,330,240,330s7.678-1.464,10.607-4.394c5.858-5.858,5.858-15.355,0-21.213L111.213,165.004z"/>
+</svg>
+
             </button>
 
             <img
@@ -104,7 +107,10 @@ const DetailComponent = ({ data }) => {
               className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white text-4xl hover:text-gray-300"
               onClick={nextImage}
             >
-              <img src="/rightarrowgallery.svg" alt="Next" />
+            <svg fill="#072F6A" height="30px" width="30px" viewBox="0 0 330.002 330.002" xmlns="http://www.w3.org/2000/svg">
+  <path d="M233.252,155.997L120.752,6.001c-4.972-6.628-14.372-7.97-21-3c-6.628,4.971-7.971,14.373-3,21 l105.75,140.997L96.752,306.001c-4.971,6.627-3.627,16.03,3,21c2.698,2.024,5.856,3.001,8.988,3.001 c4.561,0,9.065-2.072,12.012-6.001l112.5-150.004C237.252,168.664,237.252,161.33,233.252,155.997z"/>
+</svg>
+
             </button>
           </div>
         </div>
